4f5e42300f1677bf2715da5ecb4dcafbf94c3c23
Roby23sy <roby23sy@yahoo.com>
2023-12-06 20:22:44 +0200

    HA-1 Add pivot table between foods and ingridients

---
+++ database/migrations/2023_12_06_180755_create_pivot_tables_for_foods_and_ingredients.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food_ingredient_pivot', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('food_id');
+            $table->unsignedBigInteger('ingredient_id');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('food_id')->references('id')->on('foods')->onDelete('cascade');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food_ingredient_pivot');
+    }
+};
